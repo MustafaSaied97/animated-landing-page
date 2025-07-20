efdb170243a89c5bd07788fd4fc10382eac348fb
Mustafa Saied <mustafasaied97@gmail.com>
2025-07-20 12:54:51 +0300

    setup icon and name of app

--- app/app.vue
+++ app/app.vue
@@ -3,3 +3,19 @@
     <NuxtPage />
   </div>
 </template>
+<script setup>
+useHead({
+  title: 'landing page',
+  meta: [
+    { hid: 'charset', name: 'charset', content: 'utf-8' },
+    {
+      hid: 'description',
+      name: 'description',
+      content: 'landing page'
+    },
+    { hid: 'author', name: 'author', content: 'landing page' },
+    { hid: 'keywords', name: 'keywords', content: 'landing page' },
+    { hid: 'og:title', property: 'og:title', content: 'landing page' }
+  ]
+})
+</script>
