4c8ede7637f36451168f5cc6b54840175fe451e4
Mustafa Saied <mustafasaied97@gmail.com>
2025-07-20 18:24:30 +0300

    update staticMetaData

--- app/app.vue
+++ app/app.vue
@@ -4,18 +4,94 @@
   </div>
 </template>
 <script setup>
+const staticMetaData = {
+  title: 'ASTC',
+  description:
+    'ASTC has been a trusted contractor in Saudi Arabia since 2007, delivering expert-level Project Management, Telecom Engineering, and IT services.',
+  type: 'website',
+  image: '/favicon.ico', 
+  url: 'https://astc.com.sa/astc/',  
+  keywords:
+    'ASTC, Arabian for Science and Technology Co., Saudi contractor, project management, telecom, IT services',
+  author: 'ASTC Team'
+}
+
 useHead({
-  title: 'landing page',
+  title: staticMetaData.title,
   meta: [
     { hid: 'charset', name: 'charset', content: 'utf-8' },
     {
       hid: 'description',
       name: 'description',
-      content: 'landing page'
+      content: staticMetaData.description
     },
-    { hid: 'author', name: 'author', content: 'landing page' },
-    { hid: 'keywords', name: 'keywords', content: 'landing page' },
-    { hid: 'og:title', property: 'og:title', content: 'landing page' }
+    {
+      hid: 'author',
+      name: 'author',
+      content: staticMetaData.author
+    },
+    {
+      hid: 'keywords',
+      name: 'keywords',
+      content: staticMetaData.keywords
+    },
+    {
+      hid: 'og:title',
+      property: 'og:title',
+      content: staticMetaData.title
+    },
+    {
+      hid: 'og:description',
+      property: 'og:description',
+      content: staticMetaData.description
+    },
+    {
+      hid: 'og:type',
+      property: 'og:type',
+      content: staticMetaData.type
+    },
+    {
+      hid: 'og:image',
+      property: 'og:image',
+      content: staticMetaData.image
+    },
+    {
+      hid: 'og:url',
+      property: 'og:url',
+      content: staticMetaData.url
+    },
+    {
+      hid: 'twitter:title',
+      name: 'twitter:title',
+      content: staticMetaData.title
+    },
+    {
+      hid: 'twitter:description',
+      name: 'twitter:description',
+      content: staticMetaData.description
+    },
+    {
+      hid: 'twitter:image',
+      name: 'twitter:image',
+      content: staticMetaData.image
+    },
+    {
+      hid: 'twitter:url',
+      name: 'twitter:url',
+      content: staticMetaData.url
+    },
+    {
+      hid: 'twitter:card',
+      name: 'twitter:card',
+      content: 'summary_large_image'
+    }
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png'
+    }
   ]
 })
 </script>
